b3d29796b41617036f781c6042571f41ef5070ba
Mike Farah <mfarah@dius.com.au>
2014-03-28 16:17:51 +1100

    using align_faces library to normalise images

--- python/app.py
+++ python/app.py
@@ -2,6 +2,10 @@
 import facedetect
 import gabor_filter
 import csv
+import os
+import sys, math
+from PIL import Image
+import align_faces
 
 if __name__ == '__main__':
     import sys
@@ -11,12 +15,15 @@ if __name__ == '__main__':
       file_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
       for row in file_reader:
         result = facedetect.detect_faces(row[0])
+
         if len(result['face']) != 1:
             next
+
         face = result['face'][0]
-        face_vector = gabor_filter.filter_face(row[0],face[0], face[1], face[2], face[3])
+        aligned_filename = "gabor_aligned/" + os.path.basename(row[0])
+        align_faces.CropFace(Image.open(row[0]), eye_left=(face[0],face[1]), eye_right=(face[2],face[1]), offset_pct=(0.08,0.08), dest_sz=(200,200)).save(aligned_filename)
+
+        face_vector = gabor_filter.filter_face(aligned_filename,0, 0, 200, 200)
         print row[1]
         print len(face_vector)
         print face_vector
-
-
