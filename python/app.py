727b5ad730badbd95786a554724ebd4ffad245a6
David Clarke <terrorhawks@gmail.com>
2014-03-28 16:34:26 +1100

    final stuff

--- python/app.py
+++ python/app.py
@@ -6,11 +6,13 @@ import os
 import sys, math
 from PIL import Image
 import align_faces
+from sklearn import svm
 
 if __name__ == '__main__':
     import sys
 
-    training_set = []
+    X = []
+    y = []
     with open('./dataset_prep/classified_images.csv','rb') as csvfile:
       file_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
       for row in file_reader:
@@ -22,8 +24,21 @@ if __name__ == '__main__':
         face = result['face'][0]
         aligned_filename = "gabor_aligned/" + os.path.basename(row[0])
         align_faces.CropFace(Image.open(row[0]), eye_left=(face[0],face[1]), eye_right=(face[2],face[1]), offset_pct=(0.08,0.08), dest_sz=(200,200)).save(aligned_filename)
-
         face_vector = gabor_filter.filter_face(aligned_filename,0, 0, 200, 200)
         print row[1]
         print len(face_vector)
-        print face_vector
+        X <<  face_vector
+        y << row[1]
+
+    clf = svn.SVC()
+    clf.fit(X,y)
+    result = facedetect.detect_faces("cohn-kanade-images/S005/001/S005_001_00000006.png")
+    face = result['face'][0]
+    aligned_filename = "gabor_aligned/" + os.path.basename(row[0])
+    align_faces.CropFace(Image.open(row[0]), eye_left=(face[0],face[1]), eye_right=(face[2],face[1]), offset_pct=(0.08,0.08), dest_sz=(200,200)).save(aligned_filename)
+    face_vector = gabor_filter.filter_face(aligned_filename,0, 0, 200, 200)
+
+    labels = clf.predict([face_vector])
+    print labels[0]
+
+
